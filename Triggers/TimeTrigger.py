3e5d534061a68597771a9ae2c68e3d116dd34208
Luca Schmider <luca.schmider@gmail.com>
2020-08-01 11:04:50 +0200

    debounced timetrigger

--- Triggers/TimeTrigger.py
+++ Triggers/TimeTrigger.py
@@ -1,14 +1,17 @@
 from datetime import datetime, timedelta
+from time import sleep
 
 from Triggers.TriggerBase import TriggerBase
 
 
 class TimeTrigger(TriggerBase):
+    __last_trigger_time__ = None
+
     def run(self) -> None:
         trigger_time = datetime.now()
         while True:
             now = datetime.now()
-            if now + timedelta(seconds=-1) <= trigger_time <= now + timedelta(1):
+            if now + timedelta(seconds=-0.5) <= trigger_time <= now + timedelta(0.5):
                 self.trigger()
-            else:
-                print("Else")
+                self.__last_trigger_time__ = now
+            sleep(1)
